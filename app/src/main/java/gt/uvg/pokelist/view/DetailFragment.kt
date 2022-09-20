fb05e551eaf4ca2aabc1b9dba530e0bf6bcf31a8
Andres Eduardo Quezada Bendaña <andresequez@gmail.com>
2022-09-20 16:26:30 -0600

    Complete

--- app/src/main/java/gt/uvg/pokelist/view/DetailFragment.kt
+++ app/src/main/java/gt/uvg/pokelist/view/DetailFragment.kt
@@ -1,7 +1,49 @@
 package gt.uvg.pokelist.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.squareup.picasso.Picasso
+import gt.uvg.pokelist.databinding.FragmentDetailBinding
+import gt.uvg.pokelist.repository.PokemonRepository
+
 
 class DetailFragment : Fragment() {
+    companion object{
+        val IDPoke = "pokemonId"
+    }
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+    private var pokemonId: Int = 0
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let{pokemonId = it.getInt(IDPoke)}
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val pokemonList = PokemonRepository().getPokemonList()
+        val poke = pokemonList.find{it.id == pokemonId}
+        Picasso.get().load(poke!!.imageUrlFront).into(binding.imageView2)
+        Picasso.get().load(poke.imageUrlBack).into(binding.imageView3)
+        Picasso.get().load(poke.imageUrlShinnyFront).into(binding.imageView4)
+        Picasso.get().load(poke.imageUrlShinnyBack).into(binding.imageView5)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/gt/uvg/pokelist/view/PokemonListAdapter.kt
+++ app/src/main/java/gt/uvg/pokelist/view/PokemonListAdapter.kt
@@ -1,8 +1,9 @@
 package gt.uvg.pokelist.view
-
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import gt.uvg.pokelist.databinding.ItemPokemonViewBinding
 import gt.uvg.pokelist.model.Pokemon
 
@@ -16,11 +17,17 @@ class PokemonListAdapter(private val pokemonList: List<Pokemon>) : RecyclerView.
     }
 
     override fun onBindViewHolder(holder: PokemonListHolder, position: Int) {
-        // TODO
+        val poke = pokemonList.get(position)
+        holder.binding.pokemonName.text = poke.name
+        Picasso.get().load(poke.imageUrlFront).into(holder.binding.pokemonPhoto)
+
+        holder.binding.root.setOnClickListener{
+            val action = MainFragmentDirections.actionMainFragmentToDetailFragment(poke.id)
+            holder.itemView.findNavController().navigate(action)
+        }
     }
 
     override fun getItemCount(): Int {
-        // TODO
-        return 0
+        return pokemonList.size
     }
 }
